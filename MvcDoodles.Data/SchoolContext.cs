448aa3c1f697895277fca5ea368a76e63e52085b
JavDev <JGonzalezDeveloper@gmail.com>
2017-12-14 20:56:56 -0600

    adding more base work

---
+++ MvcDoodles.Data/SchoolContext.cs
@@ -0,0 +1,22 @@
+﻿using MvcDoodles.Models;
+using System.Data.Entity;
+
+namespace MvcDoodles.Data
+{
+    public class SchoolContext : DbContext
+    {
+
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ MvcDoodles/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿namespace MvcDoodles.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
